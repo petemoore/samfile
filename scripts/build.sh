470e182868ed533fd6d14a9f4ddfcd1b41a8256c
Pete Moore <pmoore@mozilla.com>
2019-09-11 18:20:06 +0200

    Run tests in build.sh script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -eu
 
-# This script is needed because `go test -covermode=atomic` cover doesn't
-# currently support being run against multiple packages
-
 cd "$(dirname "${0}")"/..
 
 go get -d -t ./...
 go install -ldflags "-X main.revision=$(git rev-parse HEAD) -X main.version=$(git tag -l 'v*.*.*' --points-at HEAD | sed -n '1s/^v//p')" github.com/petemoore/samfile/cmd/samfile
-"$(go env GOPATH)/bin/samfile" --help
+go test -v ./...
+echo
+echo "$(go env GOPATH)/bin/samfile built and tested successfully"
